bb49adef7a4ad2b4130ca627e6c10d6857b9d1de
Omar Alqassem <59409611+omaralqassem@users.noreply.github.com>
2025-06-05 14:56:37 +0300

    ..

--- Dashboard Test.py
+++ Dashboard Test.py
@@ -1,12 +1,1 @@
-url ='https://www.coingecko.com'
-import urllib.request
-try:
-    response = urllib.request.urlopen(url)
-    if response.getcode() == 200:
-        print('Bingo')
-    else:
-        print('The response code was not 200, but: {}'.format(
-            response.get_code()))
-except urllib.error.HTTPError as e:
-    print('''An error occurred: {}
-The response code was {}'''.format(e, e.getcode()))
+
